dd64ae888e9dcaf796e24dd57f1c5d3e25fece50
VORNEX-01 <mrvornexlinux@gmail.com>
2026-07-19 21:12:32 +0330

    update settings

--- vip_shooter/settings.py
+++ vip_shooter/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEFAULT_SECRET_KEY = "django-insecure-zn4o6nh+hzh^if&td538r(qmo&3y=k-+l2c^et6c#%t$vqd0fi"
 SECRET_KEY = os.environ.get("SECRET_KEY", DEFAULT_SECRET_KEY)
 
-DEBUG = os.environ.get("DEBUG", "1") == "1"
+DEBUG = True 
 
 ALLOWED_HOSTS = [h.strip() for h in os.environ.get("ALLOWED_HOSTS", "*").split(",") if h.strip()]
 
