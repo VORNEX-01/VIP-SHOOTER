8c0de9d8415bcf2dc879352ee522dadbab6ffec5
VORNEX-01 <mrvornexlinux@gmail.com>
2026-07-19 22:48:07 +0330

    add static assets

--- vip_shooter/urls.py
+++ vip_shooter/urls.py
@@ -1,8 +1,6 @@
 import os
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
 
 ADMIN_PATH = os.environ.get("ADMIN_PATH", "01-control-vipshooter").strip("/")
 
@@ -10,6 +8,3 @@ urlpatterns = [
     path("", include("vip.urls")),
     path(f"{ADMIN_PATH}/", admin.site.urls),
 ]
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
