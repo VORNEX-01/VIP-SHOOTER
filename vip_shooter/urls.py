4ea4db47ebf7ab5bb9163f5a704462fff6e219cb
VORNEX-01 <mrvornexlinux@gmail.com>
2026-07-20 19:00:48 +0330

    serve media in production

--- vip_shooter/urls.py
+++ vip_shooter/urls.py
@@ -1,15 +1,15 @@
 import os
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 ADMIN_PATH = os.environ.get("ADMIN_PATH", "01-control-vipshooter").strip("/")
 
 urlpatterns = [
     path("", include("vip.urls")),
     path(f"{ADMIN_PATH}/", admin.site.urls),
-]
 
-# برای اینکه لینک فایل‌های receipt داخل ادمین 404 نده (حتی وقتی DEBUG=0 هست)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    # Serve MEDIA in production too (Railway)
+    re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+]
