dae4ce101d7dd0fe6bf42fdadf278530efe27c26
VORNEX-01 <mrvornexlinux@gmail.com>
2026-07-20 18:54:39 +0330

    ui spacing + copy button

--- vip_shooter/urls.py
+++ vip_shooter/urls.py
@@ -1,6 +1,8 @@
 import os
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 ADMIN_PATH = os.environ.get("ADMIN_PATH", "01-control-vipshooter").strip("/")
 
@@ -8,3 +10,6 @@ urlpatterns = [
     path("", include("vip.urls")),
     path(f"{ADMIN_PATH}/", admin.site.urls),
 ]
+
+# برای اینکه لینک فایل‌های receipt داخل ادمین 404 نده (حتی وقتی DEBUG=0 هست)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
